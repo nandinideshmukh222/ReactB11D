868bce586b3ad453eb5e1f44395bb5e74251b6c0
nandinideshmukh222 <nandinideshmukh222@gmail.com>
2024-05-23 01:00:53 +0530

    react project using class component,AF,FE done
    react project using class component,AF,FE done

---
+++ my-react-project/src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+import Welcome from './Welcome';
+import ClassCompBody from './ClassCompBody';
+import ClassComponent from './ClassCompoAssign';
+import ArrowFunction from './ArrowFunAssign';
+import FunctionExpression from './FunctionExpAssign';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <h1>I am able to setup ReactJS project successfully</h1> */}
+   {/* <Welcome/> */}
+   {/* <ClassCompBody/> */}
+   {/* <ClassComponent/> */}
+     {/* <ArrowFunction/> */}
+     <FunctionExpression/>
+    </div> 
+  ); 
+}
+
+export default App;
